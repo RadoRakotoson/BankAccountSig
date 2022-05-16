a86821f829289841d0e249798b601fd7484adca3
Rado Rakotoson <rado.rakotoson@shodo.io>
2022-05-16 16:25:30 +0200

    Deposit transaction first implementation

---
+++ src/main/java/com/kata/bankaccount/Account.java
@@ -0,0 +1,13 @@
+package com.kata.bankaccount;
+
+public class Account {
+    private int balance = 0;
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
